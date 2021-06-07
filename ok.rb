fb74369f33ddae31d7f1daf72c0e186e06a296a2
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-07 09:29:43 -0700

    Brew formula update for ok version v0.1.27

--- ok.rb
+++ ok.rb
@@ -5,28 +5,28 @@
 class Ok < Formula
   desc "A task runner"
   homepage "https://github.com/broothie/ok"
-  version "0.1.26"
+  version "0.1.27"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/broothie/ok/releases/download/v0.1.26/ok_0.1.26_Darwin_x86_64.tar.gz"
-      sha256 "65dcfdb00d2a6886fdb3359e923a23a42a765bcb1aa22616fcc852ac7b956a77"
+      url "https://github.com/broothie/ok/releases/download/v0.1.27/ok_0.1.27_Darwin_x86_64.tar.gz"
+      sha256 "42b47ba85a98ce33473d00aa753d6cff7c20344d0fc4c809d25917149e3115a7"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/broothie/ok/releases/download/v0.1.26/ok_0.1.26_Darwin_arm64.tar.gz"
-      sha256 "be750a7ff81d28e6736ca3040a9861ee365e8f33707177b40f8020a4bf39b044"
+      url "https://github.com/broothie/ok/releases/download/v0.1.27/ok_0.1.27_Darwin_arm64.tar.gz"
+      sha256 "b384eaa541dd9b8e49d649fa9bacd26feade8eea34a115b5a2448adf25f4a57d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/broothie/ok/releases/download/v0.1.26/ok_0.1.26_Linux_x86_64.tar.gz"
-      sha256 "20e64c12b4469618761c9505d8c60edd9f109fbd6f2559faabbf8d7a19e29c59"
+      url "https://github.com/broothie/ok/releases/download/v0.1.27/ok_0.1.27_Linux_x86_64.tar.gz"
+      sha256 "a32c48d3a622ce42abf6edecd5a35266d79f021269f95452b32faacf9a50885b"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/broothie/ok/releases/download/v0.1.26/ok_0.1.26_Linux_arm64.tar.gz"
-      sha256 "fa21a1c8dfa98dfb717cddc3e9fb5759128541d5b12b1b071ee1d3abb0583f8c"
+      url "https://github.com/broothie/ok/releases/download/v0.1.27/ok_0.1.27_Linux_arm64.tar.gz"
+      sha256 "5c96a9a47729262745da02a74a4906eb884c1753097cb892fcd5546fa4f93d7d"
     end
   end
 
